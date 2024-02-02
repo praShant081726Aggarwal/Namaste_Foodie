bddd132351d59b2d8c2a60843c291acd374d084e
unknown <aggarwalprashant235@gmail.com>
2024-02-03 02:30:13 +0530

    Hooked Successfuly

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,21 +1,41 @@
 import Search from "./Search";
 import RestaurantCard from "./RestaurantCard";
 import restList from "../utils/storeData";
+import { useState } from "react";
 //Body
-const Body = () => { 
-    return (
-      <div className="body" id="body">
-          <div>
-          <Search />
-          </div>
-          <div className="resContainer" id="resContainer">
-          {
-            restList.map(restaurant => (
-            <RestaurantCard key={restaurant.info.id} resData={restaurant}/>))
-          }
-          </div>
-      </div>
-    );
-  };
+const Body = () => {
+  // State Variable
+  const [ListofRestaurant, setListofRestaurant] = useState(restList);
 
-  export default Body;
+  return (
+    <div className="body" id="body">
+      <div>
+        <Search />
+      </div>
+
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            // filter logic
+            const filteredList = ListofRestaurant.filter(
+              (res) => res.info.avgRatingString > 4
+            );
+            setListofRestaurant(filteredList);
+          }}
+        >
+          Top Rated Restaurant
+        </button>
+      </div>
+
+      <div className="resContainer" id="resContainer">
+        {ListofRestaurant.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
+//
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -2,12 +2,12 @@ import { CDN_RESTIMGURL } from "../utils/constants";
 //Restaurant Card
 const RestaurantCard = (props)=>{
     const {resData} = props;
-    const {name,cloudinaryImageId,avgRating,cuisines} = resData?.info;
+    const {name,cloudinaryImageId,avgRatingString,cuisines} = resData?.info;
     return (
         <div className="resCard" id="resCard">
             <img className="resImg" src={ CDN_RESTIMGURL + cloudinaryImageId}></img>
             <h3>{name}</h3>
-            <h4>{avgRating}</h4>
+            <h4>{avgRatingString}</h4>
             <h4>{resData.info.sla.deliveryTime}</h4>
             <h4>{cuisines.join(", ")}</h4>
         </div>
